7ac80d6434b8447ecb7b6ce5d209a16b6221d855
Jose Castillo <jmanuelcastillor@hotmail.com>
2023-03-02 14:31:13 -0600

    Tarea 1

---
+++ Controllers/DispositivosAlmacenamientoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiDispositivosAlmacenamiento.Entidades;
+
+namespace WebApiDispositivosAlmacenamiento.Controllers
+{
+    [ApiController]
+    [Route("api/dispositivosalmacenamiento")]
+    public class DispositivosAlmacenamientoController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<DispositivoAlmacenamiento>> Get()
+        {
+            return new List<DispositivoAlmacenamiento>()
+            {
+                new DispositivoAlmacenamiento { Id = 1, Capacidad = "2 TB", VelocidadLectura = "7400 MB/s", VelocidadEscritura = "6400 MB/s"},
+                new DispositivoAlmacenamiento { Id = 2, Capacidad = "1 TB", VelocidadLectura = "7000 MB/s", VelocidadEscritura = "6000 MB/s"}
+            };
+        }
+    }
+}
---
+++ Entidades/DispositivoAlmacenamiento.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApiDispositivosAlmacenamiento.Entidades
+{
+    public class DispositivoAlmacenamiento
+    {
+        public int Id { get; set; }
+
+        public string Capacidad { get; set; }
+
+        public string VelocidadLectura { get; set; }
+
+        public string VelocidadEscritura { get; set; }
+    }
+}
